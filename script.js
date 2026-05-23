390b67b39d70a1faf43347fe0395ffdc635f67b0
michely archetti <michelliarchetti@gmail.com>
2026-05-22 21:32:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function toggleMenu() {
-  document.getElementById("nav").classList.toggle("active");
-}
+const menu = document.querySelector(".menu");
+const nav = document.getElementById("nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+/* FECHAR CLICANDO FORA */
+document.addEventListener("click", (e) => {
+
+  const clicouNoMenu =
+    nav.contains(e.target) ||
+    menu.contains(e.target);
+
+  if (!clicouNoMenu) {
+    nav.classList.remove("active");
+  }
+});
